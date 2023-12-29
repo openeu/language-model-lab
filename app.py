41849832b17610b9c8260a758a74f32b1007d2b2
tools4eu-admin <openeuro@proton.me>
2023-12-29 11:58:03 +0100

    new data loading

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import gradio as gr
 from datasets import Dataset, load_dataset
 import pandas as pd
+from transformers import AutoTokenizer, AutoModel, DataCollatorForLanguageModeling, RobertaConfig, RobertaForMaskedLM, TrainingArguments, Trainer
 
 # dataset = Dataset.from_dict({'data': ['no data yet']})
+
+# column_names = []
+
+
+def update_dataset_columns(df_sample):
+    print("change event")
+    column_names = list(df_sample)
+    return gr.Dropdown(choices=column_names, interactive=True)
 
 def sample_from_iterable_dataset(dataset, num_samples):
     sampled_data = []
@@ -11,34 +20,82 @@ def sample_from_iterable_dataset(dataset, num_samples):
         sampled_data.append(example)
         if len(sampled_data) >= num_samples:
             break
-    
+    print(sampled_data)
     return sampled_data
 
 
 
-def load_dataset_by_name(dataset_name, dataset_config):
-    info = 'Loading...'
+# def load_dataset_by_name(dataset_name, dataset_config):
+def load_dataset_by_name(dataset_name):
+    print("Load dataset by name")
     try:
-        if dataset_config not in [None, ""]:
-            dataset = load_dataset(dataset_name, dataset_config, streaming=True)
-            info = "Loaded dataset: " + dataset_name + " with config: " + dataset_config
-        else:
-            dataset = load_dataset(dataset_name, streaming=True)
-            info = "Loaded dataset: " + dataset_name
+        global dataset
+        # if dataset_config not in [None, ""]:
+        #     dataset = load_dataset(dataset_name, dataset_config, streaming=True)
+        # else:
+        #     dataset = load_dataset(dataset_name, streaming=True)
+
+        dataset = load_dataset("parquet", data_files=dataset_name, streaming=True)
         num_samples_to_get = 10
         samples = sample_from_iterable_dataset(dataset["train"], num_samples_to_get)
-        column_names = list(samples[0].keys())
-
-        data_dict = {column: [example["text"] for example in samples] for column in column_names}
-        print(column_names)
-        dropdown_text_column.update(choices=column_names)
-        df = pd.DataFrame(data_dict)
-
+        # dropdown_text_column(choices=column_names)
+        df = pd.DataFrame(samples)
+        return df
 
     except Exception as e:
-        info = f'Error loading dataset "{dataset_name}": {str(e)}'
-        df = None
-    return info, df
+        return None
+
+def pretrain(hf_model):
+    tokenizer = AutoTokenizer.from_pretrained(hf_model)
+    model = AutoModel.from_pretrained(hf_model)
+
+    # Set a configuration for our RoBERTa model
+    config = RobertaConfig(
+        vocab_size=50265,
+        max_position_embeddings=514,
+        num_attention_heads=12,
+        num_hidden_layers=6,
+        type_vocab_size=1,
+    )
+    # Initialize the model from a configuration without pretrained weights
+    model = RobertaForMaskedLM(config=config)
+    print('Num parameters: ',model.num_parameters())
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm=True, mlm_probability=0.15,
+    )
+
+    training_args = TrainingArguments(
+        output_dir='./models',
+        overwrite_output_dir=True,
+        evaluation_strategy = 'steps',
+        num_train_epochs=1000,
+        learning_rate=1e-5,
+        weight_decay=0.01,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        save_steps=1024,
+        eval_steps=1024,
+        save_total_limit=3,
+        ignore_data_skip=True,
+        gradient_accumulation_steps=4,
+        gradient_checkpointing=True,
+        fp16=True
+    )
+    # Create the trainer for our model
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["test"],
+        #prediction_loss_only=True,
+    )
+
+    trainer.train()
+    trainer.save_model('./models/' + hf_model)
+
+
 
 with gr.Blocks() as demo:
     gr.Markdown('''
@@ -50,27 +107,28 @@ with gr.Blocks() as demo:
                 ''')
     with gr.Tab('Dataset'):
         with gr.Row():
-            textbox_dataset_name = gr.Textbox(label='Hugging Face dataset to use as training data', value='oscar', placeholder='e.g. oscar')
-            textbox_dataset_config_name = gr.Textbox(label='Dataset config (optional)', value='unshuffled_deduplicated_en', placeholder='e.g. unshuffled_deduplicated_en')
+            textbox_dataset_name = gr.Textbox(label='Parquet dataset to use as training data', placeholder='./data/raw/example.pq')
+            # textbox_dataset_name = gr.Textbox(label='Hugging Face dataset to use as training data', value='oscar', placeholder='e.g. oscar')
+            # textbox_dataset_config_name = gr.Textbox(label='Dataset config (optional)', value='unshuffled_deduplicated_en', placeholder='e.g. unshuffled_deduplicated_en')
         btn_load_dataset = gr.Button('Load dataset')
-        textbox_dataset_status = gr.Textbox(label='Status')
+        dataframe_dataset = gr.Dataframe(label = 'Dataset')
         with gr.Row():
-            dropdown_text_column = gr.Dropdown(label="Select text column")
-        dataframe_dataset = gr.Dataframe(label = 'Dataset', datatype=["number", "str"])
+            dropdown_text_column = gr.Dropdown(label="Select text column", allow_custom_value=True, interactive=True)
+        btn_load_dataset.click(load_dataset_by_name, inputs=[textbox_dataset_name], outputs=[dataframe_dataset])
+        dataframe_dataset.change(update_dataset_columns, inputs=[dataframe_dataset], outputs=[dropdown_text_column])
+        # dropdown_text_column.change(update_dataset_columns)
+
 
     with gr.Tab('Preprocessing'):
         gr.Textbox()
     
     with gr.Tab('Training'):
-        gr.Textbox(label='Hugging Face model to start from', placeholder='e.g. bert-base-uncased')
+        # global dataset
+        dataframe_processed_dataset = gr.Dataframe(label = 'Processed dataset')
+        textbox_model_name = gr.Textbox(label='Hugging Face model to start from', placeholder='e.g. bert-base-uncased')
+        btn_pretrain_model = gr.Button('Pretrain model')
     with gr.Tab('Evaluation'):
         gr.Markdown('Flip text or image files using this demo.')
-    
-    event_load_dataset = btn_load_dataset.click(
-                                fn = load_dataset_by_name, 
-                                inputs=[textbox_dataset_name, textbox_dataset_config_name], 
-                                outputs=[textbox_dataset_status, dataframe_dataset]
-                            )
 
 
 
